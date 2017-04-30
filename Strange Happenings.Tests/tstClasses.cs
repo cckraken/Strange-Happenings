5b790a63135c1007cb4780e1e2d9d7f859be0c14
Mathew Carlin <P14133205@my365.dmu.ac.uk>
2017-04-30 03:10:31 +0100

    Added tests and a test plan

---
+++ Strange Happenings.Tests/tstClasses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strange_Happenings.Models;
+
+namespace Strange_Happenings.Tests
+{
+    [TestClass]
+    public class Classes
+    {
+        [TestMethod]
+
+        public void InstanceOK()
+
+        {
+
+            //create an instance class
+
+            Genre AnAuthor = new Genre();
+
+            //test to see that it exists
+
+            Assert.IsNotNull(AnAuthor);
+            
+          
+        }
+
+        [TestMethod]
+
+        public void InstanceOK2()
+
+        {
+
+            Review AReview = new Review();
+
+            Assert.IsNotNull(AReview);
+
+                    
+        }
+
+        [TestMethod]
+
+        public void InstanceOK3()
+
+        {
+
+            Article AnArticle = new Article();
+
+            Assert.IsNotNull(AnArticle);
+
+
+        }
+
+
+
+
+
+    }
+
+}
--- Strange Happenings.Tests/tstController.cs
+++ Strange Happenings.Tests/tstController.cs
@@ -1,54 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Strange_Happenings;
 using Strange_Happenings.Controllers;
+using System.Web.Mvc;
 
-namespace Strange_Happenings.Tests2.Controllers
+namespace Strange_Happenings.Tests2
 {
-    [TestClass]
-    public class HomeControllerTest
+
+
+
+  [TestClass]
+
+    public class HomeControllerTesting
+
     {
+
         [TestMethod]
+
         public void Index()
+
         {
-            // Arrange
+
+           
+
             HomeController controller = new HomeController();
 
-            // Act
+
+
+   
+
             ViewResult result = controller.Index() as ViewResult;
 
-            // Assert
+
+            
+
             Assert.IsNotNull(result);
+
         }
 
+
+
         [TestMethod]
+
         public void About()
+
         {
+
             // Arrange
+
             HomeController controller = new HomeController();
 
+
+
             // Act
+
             ViewResult result = controller.About() as ViewResult;
 
+
+
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+
+            Assert.AreEqual("Description", result.ViewBag.Message);
+
         }
+
+
 
         [TestMethod]
+
         public void Contact()
+
         {
+
             // Arrange
+
             HomeController controller = new HomeController();
 
+
+
             // Act
+
             ViewResult result = controller.Contact() as ViewResult;
 
+
+
             // Assert
+
             Assert.IsNotNull(result);
+
         }
+
     }
+
 }
